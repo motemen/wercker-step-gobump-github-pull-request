76bf8d33532490d4bc25a69aa2a7bfb715d5898e
motemen <motemen@gmail.com>
2015-07-07 13:14:20 +0900

    token

--- run.sh
+++ run.sh
@@ -2,12 +2,14 @@
 
 set -e
 
-if [ -z "$WERCKER_GOBUMP_GITHUB_PR_GITHUB_TOKEN" ]; then
+if [ -z "$WERCKER_GOBUMP_GITHUB_PULL_REQUEST_GITHUB_TOKEN" ]; then
     fail 'you must set github_token option'
 fi
 
-label_pattern_major="$WERCKER_GOBUMP_GITHUB_PR_LABEL_PATTERN_MAJOR"
-label_pattern_minor="$WERCKER_GOBUMP_GITHUB_PR_LABEL_PATTERN_MINOR"
+github_token="$WERCKER_GOBUMP_GITHUB_PULL_REQUEST_GITHUB_TOKEN"
+
+label_pattern_major="$WERCKER_GOBUMP_GITHUB_PULL_REQUEST_LABEL_PATTERN_MAJOR"
+label_pattern_minor="$WERCKER_GOBUMP_GITHUB_PULL_REQUEST_LABEL_PATTERN_MINOR"
 
 # Install tools
 go get github.com/motemen/gobump/cmd/gobump
@@ -22,7 +24,7 @@ git config --global user.name  'werckerbot'
 pr_number=$(git show --pretty=%s | sed -n 's/^Merge pull request #\([0-9]\{1,\}\) .*/\1/p')
 
 if [ -n "$pr_number" ]; then
-  labels=$(curl -s -H "Authorization: token $WERCKER_GOBUMP_GITHUB_PR_GITHUB_TOKEN" \
+  labels=$(curl -s -H "Authorization: token $github_token" \
       "https://api.github.com/repos/$WERCKER_GIT_OWNER/$WERCKER_GIT_REPOSITORY/issues/$pr_number/labels" | ./jq -r 'map(.name) | join("\n")')
 
   command='patch'
@@ -36,7 +38,7 @@ if [ -n "$pr_number" ]; then
 
   if ! git diff --exit-code; then
       git commit -a -m "bump version to $new_version\n\n$WERCKER_DEPLOY_URL"
-      git push -q "https://$GITHUB_TOKEN@github.com/$WERCKER_GIT_OWNER/$WERCKER_GIT_REPOSITORY" HEAD:master >/dev/null 2>&1 # supperss unexpected token output
+      git push -q "https://$github_token@github.com/$WERCKER_GIT_OWNER/$WERCKER_GIT_REPOSITORY" HEAD:master >/dev/null 2>&1 # supperss unexpected token output
   fi
 else
   info 'no pull request merged, will not bump version'
