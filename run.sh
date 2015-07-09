db6291cff66241f13bf91c466a221aaba7489dd6
motemen <motemen@gmail.com>
2015-07-09 20:46:24 +0900

    fix

--- run.sh
+++ run.sh
@@ -25,29 +25,25 @@ git config --global user.name  'werckerbot'
 pr_number=$(git show --pretty=%s | sed -n 's/^Merge pull request #\([0-9]\{1,\}\) .*/\1/p')
 
 if [ -n "$pr_number" ]; then
-  # Check if specified files are changed
-  if [ -n "$changed_files_pattern" ]; then
-    if ! ( git diff --name-only HEAD~1 | grep -q -i -E "$changed_files_pattern" ); then
-      info "no files matching '$changed_files_pattern' changed"
-      exit 0
+  if [ -z "$changed_files_pattern" ] || ( git diff --name-only HEAD~1 | grep -q -i -E "$changed_files_pattern" ); then
+    labels=$(curl -s -H "Authorization: token $github_token" \
+        "https://api.github.com/repos/$WERCKER_GIT_OWNER/$WERCKER_GIT_REPOSITORY/issues/$pr_number/labels" | ./jq -r 'map(.name) | join("\n")')
+
+    command='patch'
+    if [ -n "$label_pattern_major" ] && ( echo "$labels" | grep -q -i -E "$label_pattern_major" ) then
+      command='major'
+    elif [ -n "$label_pattern_minor" ]  && ( echo "$labels" | grep -q -i -E "$label_pattern_minor" ) then
+      command='minor'
     fi
-  fi
 
-  labels=$(curl -s -H "Authorization: token $github_token" \
-      "https://api.github.com/repos/$WERCKER_GIT_OWNER/$WERCKER_GIT_REPOSITORY/issues/$pr_number/labels" | ./jq -r 'map(.name) | join("\n")')
+    new_version=$(gobump $command -w -v | ./jq -r '.[]')
 
-  command='patch'
-  if [ -n "$label_pattern_major" ] && ( echo "$labels" | grep -q -i -E "$label_pattern_major" ) then
-    command='major'
-  elif [ -n "$label_pattern_minor" ]  && ( echo "$labels" | grep -q -i -E "$label_pattern_minor" ) then
-    command='minor'
-  fi
-
-  new_version=$(gobump $command -w -v | ./jq -r '.[]')
-
-  if ! git diff --exit-code; then
-    git commit -a -m "bump version to $new_version"$'\n\n'"$WERCKER_DEPLOY_URL"
-    git push -q "https://$github_token@github.com/$WERCKER_GIT_OWNER/$WERCKER_GIT_REPOSITORY" HEAD:master >/dev/null 2>&1 # supperss unexpected token output
+    if ! git diff --exit-code; then
+      git commit -a -m "bump version to $new_version"$'\n\n'"$WERCKER_DEPLOY_URL"
+      git push -q "https://$github_token@github.com/$WERCKER_GIT_OWNER/$WERCKER_GIT_REPOSITORY" HEAD:master >/dev/null 2>&1 # supperss unexpected token output
+    fi
+  else
+    info "no files matching '$changed_files_pattern' changed"
   fi
 else
   info 'no pull request merged, will not bump version'
